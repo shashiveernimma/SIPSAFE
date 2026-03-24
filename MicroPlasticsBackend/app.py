9541514e53fc34498ea126345f78a3ff7fd9b543
shashiveernimma <shashiveernimma@gmail.com>
2026-03-24 14:09:17 +0530

    Updated app.py

--- MicroPlasticsBackend/app.py
+++ MicroPlasticsBackend/app.py
@@ -132,6 +132,10 @@ def analyze():
 
 # ===============================
 # RUN SERVER
+# ======================
+# ===============================
+# RUN SERVER
 # ===============================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
